c5d14d3e9406ec53c49ca713f1c38512ff5c0a71
DanielTotev <dtotev1@gmail.com>
2017-07-02 15:40:51 +0300

    Added some more sloved problems

---
+++ MatchHexadecimalNumbers/MatchHexadecimalNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MatchHexadecimalNumbers
+{
+    public class MatchHexadecimalNumbers
+    {
+        public static void Main()
+        {
+            var pattern = @"\b(?:0x)?[0-9A-F]+\b";
+            var numbers = Console.ReadLine();
+            var matches = Regex.Matches(numbers, pattern);
+            var matchedNumbers = matches.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+            Console.WriteLine(string.Join(" ", matchedNumbers));
+        }
+    }
+}
---
+++ MatchNumbers/NumbersMatcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchNumbers
+{
+    public class NumbersMatcher
+    {
+        public static void Main()
+        {
+            var pattern = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))";
+            var input = Console.ReadLine();
+            var matches = Regex.Matches(input, pattern);
+            foreach (Match numberMatch in matches)
+            {
+                Console.Write(numberMatch.Value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
